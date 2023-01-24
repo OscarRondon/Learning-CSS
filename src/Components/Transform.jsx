b71bb970ec80a8b62cf46cc224a56c8069d60d66
orondon <oscar.rondon.c@gmail.com>
2023-01-24 09:04:31 -0300

    Third Session part 2

---
+++ src/Components/Transform.jsx
@@ -0,0 +1,16 @@
+import styles from './Transform.module.css'
+
+export const Transform = () =>{
+    
+    return (
+        <>
+            <br/>
+            <div id='container' className={styles.container}>
+            </div>
+            <div id='container2' className={styles.container2}>
+            </div>
+            <br/>
+            <hr/>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { ColorProperty } from './Components/ColorProperty'
 import { LableSelectors } from './Components/LableSelectors'
 import { ClassSelectors } from './Components/ClassSelectors'
 import { Buttons } from './Components/Buttons'
+import { Transform } from './Components/Transform'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -17,6 +18,8 @@ root.render(
     <ClassSelectors/>
     
     <Buttons/>
+    
+    <Transform/>
     
   </React.StrictMode>,
 )
