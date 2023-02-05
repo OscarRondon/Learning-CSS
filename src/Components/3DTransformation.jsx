069c83778bb803f782f9acf4cf6e6acfea09656f
orondon <oscar.rondon.c@gmail.com>
2023-02-04 22:14:45 -0300

    [PseudoElements, functions, Position]

--- src/Components/3DTransformation.jsx
+++ src/Components/3DTransformation.jsx
@@ -4,31 +4,29 @@ export const Transformation3D = () => {
   return (
     <>
       <br />
-      <centre>
-        <div className={styles.mesurements}>
-          <h1>3D Transformation</h1>
-          <h2>3D Transformation</h2>
-          <h3>3D Transformation</h3>
-          <h4>3D Transformation</h4>
-          <p>Texto de prueba</p>
+      <div className={styles.mesurements}>
+        <h1>3D Transformation</h1>
+        <h2>3D Transformation</h2>
+        <h3>3D Transformation</h3>
+        <h4>3D Transformation</h4>
+        <p>Texto de prueba</p>
+      </div>
+      <div id='envelope' className={styles.envelope}>
+        <div className={styles.cube}>
+          {/* front */}
+          <div />
+          {/* back */}
+          <div />
+          {/* left */}
+          <div />
+          {/* right */}
+          <div />
+          {/* top */}
+          <div />
+          {/* bottom */}
+          <div />
         </div>
-        <div id='envelope' className={styles.envelope}>
-          <div className={styles.cube}>
-            {/* front */}
-            <div />
-            {/* back */}
-            <div />
-            {/* left */}
-            <div />
-            {/* right */}
-            <div />
-            {/* top */}
-            <div />
-            {/* bottom */}
-            <div />
-          </div>
-        </div>
-      </centre>
+      </div>
       <br />
       <hr />
     </>
